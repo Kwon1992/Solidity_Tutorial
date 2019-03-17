0f6fb80f1b16fdcf5c83e41e371d1d837144fa6d
kwon hyunwoo <khw6428@gmail.com>
2019-03-17 14:04:45 +0900

    Create OverflowAndUnderflow.sol

---
+++ OverflowAndUnderflow.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.24;
+
+contract OverflowAndUnderflow {
+    function overflow() public pure returns (uint256) {
+        uint256 max = 2**256-1;
+        return max + 1;
+    }
+    
+    function underflow() public pure returns (uint256) {
+        uint256 min = 0;
+        return min - 1;
+    }
+}
